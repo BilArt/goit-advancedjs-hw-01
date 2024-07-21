c0fbfd44e0ef0dd35d693049d67be6ad24aba8b6
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-07-21 23:20:07 +0200

    updated gallery

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,17 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+import { resolve } from 'path';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-        },
+export default defineConfig({
+  root: './src/public',
+  build: {
+    outDir: '../../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/public/1-gallery.html'),
       },
-      outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
+  },
+  server: {
+    open: '/1-gallery.html',
+  },
 });
